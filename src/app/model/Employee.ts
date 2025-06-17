e7ab47b2ec6a50878f51f7528ec87aa2f343c1af
pratiksha200009raut <pratiksha200009raut@gmail.com>
2025-06-17 17:49:05 +0530

    ui

---
+++ src/app/model/Employee.ts
@@ -0,0 +1,19 @@
+export class EmployeeModel {
+ employeeId: number;
+ name: string;
+ contactNo: string;
+ address: string;
+ city: string;
+ state: string;
+ emailId:string;
+ constructor() {
+    this.name = "";
+    this.contactNo = "";
+    this.address = "";
+    this .city ="";
+    this.state = "";
+    this.emailId ="";
+    this.employeeId = 0;
+
+}
+}
