cd60b2c219d7bc6c14aaa86e9b4f98e3a79fe832
pratiksha200009raut <pratiksha200009raut@gmail.com>
2025-06-25 18:18:51 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeModel } from './model/Employee';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_18_crud';
-}
+  employeeeForm?: FormGroup;
+
+  employeeobj:EmployeeModel =new EmployeeModel();
+  constructor(){
+    this.createForm();
+    
+  }
+
+  createForm(): void {
+    this.employeeeForm = new FormGroup({
+      name: new FormControl(this.employeeobj.name),
+      email: new FormControl(this.employeeobj.emailId),
+      employeeId: new FormControl(this.employeeobj.employeeId),
+      city: new FormControl(this.employeeobj.city),
+      state: new FormControl(this.employeeobj.state),
+      contactNo: new FormControl(this.employeeobj.contactNo),
+      address: new FormControl(this.employeeobj.address)
+    });
+  }
+  onsave() {
+    debugger;
+    const oldData =localStorage.getItem('employeeData');
+    if(oldData !== null){
+      const parseData: any[] = JSON.parse(oldData);
+      this.employeeeForm?.controls['empid'].setValue(parseData.length +1);
+    }else{
+      // Store the first employee entry in localStorage as an array
+      localStorage.setItem('employeeData', JSON.stringify([this.employeeeForm?.value]));
+      
+    }
+
+    }
+
+  }
